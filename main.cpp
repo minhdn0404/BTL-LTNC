f0bc8147329e6110898268bd6dc425b5dc90e597
minhdn0404 <87870788+minhdn0404@users.noreply.github.com>
2022-04-15 09:10:31 +0700

    Update main.cpp
    main commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 
 
+
+
 #include <iostream>
 #include <SDL.h>
 #include <string>
@@ -18,6 +20,7 @@ const int FRAME_UP = 40;
 const int FRAME_DOWN = 20;
 const int FRAME_LEFT = 20;
 const int FRAME_RIGHT = 20;
+const int MAX_SCORE = 60;   // for snake vs snake mode
 
 void initSDL(SDL_Window* &window, SDL_Renderer* &renderer);
 void logSDLError(std::ostream& os,
@@ -44,16 +47,26 @@ struct Dot
         x = _x;
         y = _y;
     }
-    void draw ()
+    void draw (int r, int g, int b)
     {
-       SDL_SetRenderDrawColor(renderer,0,255,0,255);  // green
+       SDL_SetRenderDrawColor(renderer,r,g,b,255);  // green
        SDL_Rect dot = {x,y,size,size};
        SDL_RenderFillRect(renderer,&dot);
     }
-    void move()  // chỉ với body[0]
+    void move(string mode)  // chỉ với body[0]
     {
-        x+=directionX;
-        y+=directionY;
+        if (mode == "classic") {
+            x+=directionX;
+            y+=directionY;
+            if (x == SCREEN_WIDTH-FRAME_RIGHT) x = FRAME_LEFT;       // cạnh phải
+            if (x == FRAME_LEFT-size) x = SCREEN_WIDTH-FRAME_RIGHT-size;      // cạnh trái
+            if (y == SCREEN_HEIGHT-FRAME_DOWN) y = FRAME_UP;                    // cạnh dưới
+            if (y == FRAME_UP - size) y = SCREEN_HEIGHT-FRAME_DOWN-size;    // cạnh trên
+        }
+        else if (mode == "modern") {
+            x+=directionX;
+            y+=directionY;
+        }
     }
     void  turn_right ()
     {
@@ -85,37 +98,57 @@ struct Dot
 struct Snake
 {
     vector<Dot> body ;
+    int score;
     bool right;
     bool left;
     bool up;
     bool down;
 
-    void draw ()
+    void draw (int r,int g, int b)
     {
         for (int i=0; i<body.size(); ++i)
         {
-            body[i].draw();
+            body[i].draw(r,g,b);
         }
     }
 
-    void move ()
+    void move (string mode)
     {
-        for (int i=0; i<body.size(); ++i)
-        {
-            if (i==0) {
-                body[i].x0 = body[i].x;
-                body[i].y0 = body[i].y;
-                body[i].move();
-                //cout <<"body: " << i << ":" << body[i].x0 << " " <<  body[i].y0 << " " << body[i].x << " " <<  body[i].y << endl;
-            }
-            else {
-               body[i].x0 = body[i].x;
-               body[i].y0 = body[i].y;
-               body[i].x = body[i-1].x0;
-               body[i].y = body[i-1].y0;
-             //  cout <<"body: " << i << ":" << body[i].x0 << " " <<  body[i].y0 << " " << body[i].x << " " <<  body[i].y << endl;
+        if (mode == "classic") {
+            for (int i=0; i<body.size(); ++i)
+            {
+               if (i==0) {
+                 body[i].x0 = body[i].x;
+                 body[i].y0 = body[i].y;
+                 body[i].move("classic");
+
+               }
+               else {
+                 body[i].x0 = body[i].x;
+                 body[i].y0 = body[i].y;
+                 body[i].x = body[i-1].x0;
+                 body[i].y = body[i-1].y0;
+               }
             }
         }
+        else if (mode == "modern") {
+            for (int i=0; i<body.size(); ++i)
+            {
+               if (i==0) {
+                  body[i].x0 = body[i].x;
+                  body[i].y0 = body[i].y;
+                  body[i].move("modern");
+
+               }
+               else {
+                  body[i].x0 = body[i].x;
+                  body[i].y0 = body[i].y;
+                  body[i].x = body[i-1].x0;
+                  body[i].y = body[i-1].y0;
+               }
+            }
+        }
+
     }
 
     void turn (int direct)
@@ -191,6 +224,14 @@ struct Snake
         }
         return false;
     }
+    bool eat_other (Snake &other_snake)      // for snake vs snake mode
+    {
+        for (int i=0; i<other_snake.body.size(); ++i)
+        {
+            if (body[0].x == other_snake.body[i].x && body[0].y == other_snake.body[i].y) return true;
+        }
+        return false;
+    }
 
 };
 
@@ -224,7 +265,6 @@ struct Food
 
 struct Game
 {
-    int score;
     int high_score;
 
     void draw_frame ()
@@ -233,29 +273,40 @@ struct Game
       SDL_Rect frame_left = {0,0,FRAME_LEFT,SCREEN_HEIGHT};
       SDL_Rect frame_down = {0,SCREEN_HEIGHT-FRAME_DOWN,SCREEN_WIDTH,FRAME_DOWN};
       SDL_Rect frame_right = {SCREEN_WIDTH-FRAME_RIGHT,0,FRAME_RIGHT,SCREEN_HEIGHT};
-      SDL_SetRenderDrawColor(renderer,0,0,255,255);
+      SDL_SetRenderDrawColor(renderer,172, 249, 255, 255);
       SDL_RenderFillRect(renderer,&frame_up);
       SDL_RenderFillRect(renderer,&frame_down);
       SDL_RenderFillRect(renderer,&frame_left);
       SDL_RenderFillRect(renderer,&frame_right);
     }
 
-    bool is_Over (Snake &snake)
+    bool is_Over (Snake &snake, string mode)
     {
-       if ( (snake.body[0].inside(FRAME_LEFT,FRAME_UP,SCREEN_WIDTH-FRAME_RIGHT,SCREEN_HEIGHT-FRAME_DOWN)==false)
+       if (mode == "classic") {
+           if ( (snake.eat_itself() == true) ) return true;
+           else return false;
+       }
+       else if (mode == "modern") {
+           if ( (snake.body[0].inside(FRAME_LEFT,FRAME_UP,SCREEN_WIDTH-FRAME_RIGHT,SCREEN_HEIGHT-FRAME_DOWN)==false)
              || (snake.eat_itself() == true) ) return true;
-       // || (snake.eat_itself() == true)
+           else return false;
+       }
+    }
+    bool is_Over_svs (Snake &snake1, Snake &snake2)
+    {
+       if (snake1.eat_other(snake2)==true || snake2.eat_other(snake1)==true
+           || snake1.score == MAX_SCORE || snake2.score == MAX_SCORE) return true;
        else return false;
     }
 };
 
 
-void init_game (Snake &snake)
+void init_game (Snake &snake,int x1,int y1,int x2,int y2,int x3,int y3,int x4,int y4)
 {
-    Dot d0(FRAME_LEFT+DOT_SIZE*4,FRAME_UP+2*DOT_SIZE);   // head
-    Dot d1(FRAME_LEFT+DOT_SIZE*3,FRAME_UP+2*DOT_SIZE);
-    Dot d2(FRAME_LEFT+DOT_SIZE*2,FRAME_UP+2*DOT_SIZE);
-    Dot d3(FRAME_LEFT+DOT_SIZE,FRAME_UP+2*DOT_SIZE);
+    Dot d0(x1,y1);   // head
+    Dot d1(x2,y2);
+    Dot d2(x3,y3);
+    Dot d3(x4,y4);
     snake.body.push_back(d0);
     snake.body.push_back(d1);
     snake.body.push_back(d2);
@@ -266,36 +317,28 @@ void init_game (Snake &snake)
     snake.left = 0;
 }
 
-int main (int argc, char* argv[])
+void classic ()
 {
-    initSDL(window, renderer);
-
-    SDL_SetRenderDrawColor(renderer,255,255,255,255);
-    SDL_RenderClear(renderer);
-
-    srand(time(0));
     Game new_game;
     Snake snake;
-    init_game(snake);
-    new_game.score = 0;
-    //snake.draw();
+    init_game(snake,60,50,50,50,40,50,30,50);
+    snake.score = 0;
     Food food;
     food.random_generate();
 
-
-    while (new_game.is_Over(snake) == false)
+    while (new_game.is_Over(snake,"classic") == false)
     {
-         snake.move();
+         snake.move("classic");
          if (food.is_eaten_by(snake)==true) {
-            ++new_game.score;
-            cout << "Score: " << new_game.score << endl;
+            ++snake.score;
+            cout << "Score: " << snake.score << endl;
             snake.update();  // tăng kích thước rắn
             food.random_generate();   // random thức ăn
          }
          SDL_SetRenderDrawColor(renderer,255,255,255,255);
          SDL_RenderClear(renderer);
          new_game.draw_frame();
-         snake.draw();
+         snake.draw(0,255,0);
          food.draw_food();
          SDL_RenderPresent(renderer);
          SDL_Delay(DELAY_TIME);
@@ -313,10 +356,157 @@ int main (int argc, char* argv[])
                 default: break;
             }
          }
-         //snake.move();
 
     }
     cout << "Game Over";
+}
+
+void modern ()
+{
+    Game new_game;
+    Snake snake;
+    init_game(snake,60,50,50,50,40,50,30,50);
+    snake.score = 0;
+    Food food;
+    food.random_generate();
+
+
+    while (new_game.is_Over(snake,"modern") == false)
+    {
+         snake.move("modern");
+         if (food.is_eaten_by(snake)==true) {
+            ++snake.score;
+            cout << "Score: " << snake.score << endl;
+            snake.update();  // tăng kích thước rắn
+            food.random_generate();   // random thức ăn
+         }
+         SDL_SetRenderDrawColor(renderer,255,255,255,255);
+         SDL_RenderClear(renderer);
+         new_game.draw_frame();
+         snake.draw(0,255,0);
+         food.draw_food();
+         SDL_RenderPresent(renderer);
+         SDL_Delay(DELAY_TIME);
+
+         SDL_Event e;
+         if (SDL_PollEvent(&e)==0) continue;
+         else if (e.type == SDL_QUIT) break;
+         else if (e.type == SDL_KEYDOWN){
+            switch (e.key.keysym.sym)
+            {
+                case SDLK_RIGHT: snake.turn(6); break;
+                case SDLK_LEFT: snake.turn(4); break;
+                case SDLK_UP: snake.turn(8); break;
+                case SDLK_DOWN: snake.turn(2); break;
+                default: break;
+            }
+         }
+    }
+    cout << "Game Over";
+}
+
+void snake_vs_snake ()
+{
+    Game new_game;
+    Snake snake1;
+    Snake snake2;
+    init_game(snake1,60,50,50,50,40,50,30,50);
+    init_game(snake2,60,100,50,100,40,100,30,100);
+    snake1.score = 0;
+    snake2.score = 0;
+    Food food;
+    food.random_generate();
+
+    while (new_game.is_Over_svs(snake1, snake2) == false)
+    {
+         snake1.move("classic");
+         snake2.move("classic");
+         if (food.is_eaten_by(snake1)==true ) {
+            ++snake1.score;
+            cout << "Score1: " << snake1.score << endl;
+            snake1.update();  // tăng kích thước rắn
+            food.random_generate();   // random thức ăn
+         }
+         if (food.is_eaten_by(snake2)==true ) {
+            ++snake2.score;
+            cout << "Score2: " << snake2.score << endl;
+            snake2.update();  // tăng kích thước rắn
+            food.random_generate();   // random thức ăn
+         }
+         SDL_SetRenderDrawColor(renderer,255,255,255,255);
+         SDL_RenderClear(renderer);
+         new_game.draw_frame();
+         snake1.draw(0,255,0);
+         snake2.draw(115, 36, 254);
+         food.draw_food();
+         SDL_RenderPresent(renderer);
+         SDL_Delay(DELAY_TIME);
+
+         SDL_Event e;
+         if (SDL_PollEvent(&e)==0) continue;
+         else if (e.type == SDL_QUIT) break;
+         else if (e.type == SDL_KEYDOWN){
+            switch (e.key.keysym.sym)
+            {
+                case SDLK_RIGHT: snake1.turn(6); break;
+                case SDLK_LEFT: snake1.turn(4); break;
+                case SDLK_UP: snake1.turn(8); break;
+                case SDLK_DOWN: snake1.turn(2); break;
+                case SDLK_d: snake2.turn(6); break;
+                case SDLK_a: snake2.turn(4); break;
+                case SDLK_w: snake2.turn(8); break;
+                case SDLK_s: snake2.turn(2); break;
+                default: break;
+            }
+         }
+    }
+
+    if (snake1.score == MAX_SCORE || snake2.score == MAX_SCORE)
+    {
+        if (snake1.score > snake2.score) cout << "Snake 1 win" << endl;
+        else cout << "Snake 2 win" << endl;
+    }
+    else {
+        if (snake1.eat_other(snake2)==true) cout << "Snake 2 win" << endl;
+        else if (snake2.eat_other(snake1) == true) cout << "Snake 1 win" << endl;
+     }
+    cout << "Game Over";
+}
+
+int main (int argc, char* argv[])
+{
+    initSDL(window, renderer);
+
+    SDL_SetRenderDrawColor(renderer,255,255,255,255);
+    SDL_RenderClear(renderer);
+
+    srand(time(0));
+
+    cout << "Choose mode: " << endl;
+    cout << "1: Classic" << endl;
+    cout << "2: Modern" << endl;
+    cout << "3: Snake vs Snake" << endl;
+
+    SDL_Event e;
+    while (true) {
+        if (SDL_PollEvent(&e)==0) continue;
+        else if (e.type == SDL_QUIT) break;
+        else if (e.type == SDL_KEYDOWN) {
+          switch (e.key.keysym.sym)
+          {
+              case SDLK_1: classic(); break;
+              case SDLK_2: modern(); break;
+              case SDLK_3: snake_vs_snake(); break;
+              default: break;
+          }
+          break;
+        }
+    }
+    //classic();
+
+    //modern();
+
+    //snake_vs_snake();
 
     waitUntilKeyPressed();
     quitSDL(window, renderer);
