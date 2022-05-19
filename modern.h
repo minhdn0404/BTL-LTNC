5ef4ebfd3101ca36b693d610562339ee492cc689
minhdn0404 <87870788+minhdn0404@users.noreply.github.com>
2022-05-19 21:46:44 +0700

    Update modern.h
    modern commit

--- modern.h
+++ modern.h
@@ -6,7 +6,6 @@ void modern ()
     Game new_game;
     Snake snake;
     init_game(snake,FRAME_LEFT+40,FRAME_UP+10,FRAME_LEFT+30,FRAME_UP+10,FRAME_LEFT+20,FRAME_UP+10,FRAME_LEFT+10,FRAME_UP+10);
-    snake.score = 0;
     Food food;
     food.random_generate();
     int index_food_path = 0;
@@ -15,11 +14,12 @@ void modern ()
     {
          snake.move("modern");
          if (food.is_eaten_by(snake)==true) {
-            snake.update_score(RANDOM_FOOD_POSSIBILITY[index_food_path]);  // tăng điểm dựa vào thức ăn
+            snake.increase_score(RANDOM_FOOD_POSSIBILITY[index_food_path]);  // tăng điểm dựa vào thức ăn
             cout << "Score: " << snake.score << endl;
             snake.update();  // tăng kích thước rắn
             index_food_path = random(0,19);
             food.random_generate();   // random thức ăn
+            food.random_food_type();
          }
          SDL_SetRenderDrawColor(renderer,255,255,255,255);
          SDL_RenderClear(renderer);
@@ -27,9 +27,9 @@ void modern ()
          new_game.draw_frame(48, 92, 240,renderer);
          new_game.draw_banner(29, 243, 10, "Modern",renderer, gFont);
          new_game.draw_score(snake.score,255,0,0,SCREEN_WIDTH/2,20,200,50,renderer, gFont);
-         new_game.draw_updated_score(RANDOM_FOOD_POSSIBILITY[index_food_path],255,0,0,SCREEN_WIDTH - 60, 20, 50, 50,renderer, gFont);
+         new_game.draw_updated_score(1,RANDOM_FOOD_POSSIBILITY[index_food_path],255,0,0,SCREEN_WIDTH - 60, 20, 50, 50,renderer, gFont);
          snake.draw(0,255,0,"Background/green_head.png",renderer);
-         food.draw_food(index_food_path,renderer);
+         food.draw_food(renderer);
          SDL_RenderPresent(renderer);
          SDL_Delay(DELAY_TIME);
 
