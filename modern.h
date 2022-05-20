25ff5480e9d94b5a9cb9d66345b16bab3a6b7dd2
minhdn0404 <87870788+minhdn0404@users.noreply.github.com>
2022-05-20 16:40:08 +0700

    Update modern.h
    game commit

--- modern.h
+++ modern.h
@@ -1,16 +1,20 @@
 
 
 #pragma once
+#include "obstacle.h"
+
 void modern ()
 {
     Game new_game;
     Snake snake;
     init_game(snake,FRAME_LEFT+40,FRAME_UP+10,FRAME_LEFT+30,FRAME_UP+10,FRAME_LEFT+20,FRAME_UP+10,FRAME_LEFT+10,FRAME_UP+10);
     Food food;
-    food.random_generate();
     int index_food_path = 0;
+    vector<Obstacle> obstacles;
+    int row_state = 1;
+    food.random_generate_modern(snake,obstacles);
 
-    while (new_game.is_Over(snake,"modern") == false)
+    while (new_game.is_Over(snake,obstacles) == false)
     {
          snake.move("modern");
          if (food.is_eaten_by(snake)==true) {
@@ -18,8 +22,13 @@ void modern ()
             cout << "Score: " << snake.score << endl;
             snake.update();  // tăng kích thước rắn
             index_food_path = random(0,19);
-            food.random_generate();   // random thức ăn
+            food.random_generate_modern(snake,obstacles);   // random thức ăn
             food.random_food_type();
+
+            Obstacle obs;
+            obs.random_generate_modern(row_state,snake);
+            obstacles.push_back(obs);
+            cout << "Size: " << obstacles.size() << endl;
          }
          SDL_SetRenderDrawColor(renderer,255,255,255,255);
          SDL_RenderClear(renderer);
@@ -29,9 +38,15 @@ void modern ()
          new_game.draw_score(snake.score,255,0,0,SCREEN_WIDTH/2,20,200,50,renderer, gFont);
          new_game.draw_updated_score(1,RANDOM_FOOD_POSSIBILITY[index_food_path],255,0,0,SCREEN_WIDTH - 60, 20, 50, 50,renderer, gFont);
          snake.draw(0,255,0,"Background/green_head.png",renderer);
+
+         for (int i=0; i<obstacles.size(); ++i)
+         {
+             obstacles[i].draw(255, 255, 4,renderer);
+         }
+
          food.draw_food(renderer);
          SDL_RenderPresent(renderer);
-         SDL_Delay(DELAY_TIME);
+         SDL_Delay(DELAY_TIME+10);
 
          SDL_Event e;
          if (SDL_PollEvent(&e)==0) continue;
