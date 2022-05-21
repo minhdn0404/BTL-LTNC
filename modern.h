d86e6e38ca8eda5fa1cc11454015f5753df1ff97
minhdn0404 <87870788+minhdn0404@users.noreply.github.com>
2022-05-21 12:54:10 +0700

    Update modern.h
    game commit

--- modern.h
+++ modern.h
@@ -9,34 +9,40 @@ void modern ()
     Snake snake;
     init_game(snake,FRAME_LEFT+40,FRAME_UP+10,FRAME_LEFT+30,FRAME_UP+10,FRAME_LEFT+20,FRAME_UP+10,FRAME_LEFT+10,FRAME_UP+10);
     Food food;
-    int index_food_path = 0;
     vector<Obstacle> obstacles;
     int row_state = 1;
-    food.random_generate_modern(snake,obstacles);
+    food.random_generate(snake,obstacles);
 
+    bool eat_food_check = 0;
     while (new_game.is_Over(snake,obstacles) == false)
     {
          snake.move("modern");
+         int food_index;
          if (food.is_eaten_by(snake)==true) {
-            snake.increase_score(RANDOM_FOOD_POSSIBILITY[index_food_path]);  // tăng điểm dựa vào thức ăn
+            eat_food_check = 1;
+            snake.increase_score(food.current_index_food_type);  // tăng điểm dựa vào thức ăn
             cout << "Score: " << snake.score << endl;
             snake.update();  // tăng kích thước rắn
-            index_food_path = random(0,19);
-            food.random_generate_modern(snake,obstacles);   // random thức ăn
+            food_index = food.current_index_food_type;
+            food.random_generate(snake,obstacles);   // random thức ăn
             food.random_food_type();
 
             Obstacle obs;
-            obs.random_generate_modern(row_state,snake);
+            obs.random_generate(row_state,snake);
             obstacles.push_back(obs);
             cout << "Size: " << obstacles.size() << endl;
          }
+
          SDL_SetRenderDrawColor(renderer,255,255,255,255);
          SDL_RenderClear(renderer);
          new_game.draw_play_background(renderer);
          new_game.draw_frame(48, 92, 240,renderer);
          new_game.draw_banner(29, 243, 10, "Modern",renderer, gFont);
          new_game.draw_score(snake.score,255,0,0,SCREEN_WIDTH/2,20,200,50,renderer, gFont);
-         new_game.draw_updated_score(1,RANDOM_FOOD_POSSIBILITY[index_food_path],255,0,0,SCREEN_WIDTH - 60, 20, 50, 50,renderer, gFont);
+         if (eat_food_check = 1) {
+            new_game.draw_updated_score(1,food_index,255,0,0,SCREEN_WIDTH - 60, 20, 50, 50,renderer, gFont);
+         }
+         //new_game.draw_updated_score(1,RANDOM_FOOD_POSSIBILITY[index_food_path],255,0,0,SCREEN_WIDTH - 60, 20, 50, 50,renderer, gFont);
          snake.draw(0,255,0,"Background/green_head.png",renderer);
 
          for (int i=0; i<obstacles.size(); ++i)
