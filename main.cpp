b47cf74128859f73399fd412a170822abf6f6251
minhdn0404 <87870788+minhdn0404@users.noreply.github.com>
2022-05-24 02:10:34 +0700

    Update main.cpp
    game commit

--- main.cpp
+++ main.cpp
@@ -1,58 +1,26 @@
 
 
-#include <iostream>
-#include <SDL.h>
-#include <SDL_ttf.h>
-#include <SDL_image.h>
-#include <string>
-#include <vector>
-#include <ctime>
-#include "tools.h"
-#include "other.h"
-#include "SDL_util.h"
-#include "Dot.h"
-#include "Snake.h"
-#include "Food.h"
-#include "Bomb.h"
-#include "Game.h"
-#include "init.h"
-#include "classic.h"
-#include "modern.h"
-#include "snake_vs_snake.h"
-#include "timer.h"
-#include "obstacle.h"
+#include "library.h"
 
 using namespace std;
 
-void intro ()
+void mode (Button classic_button, Button modern_button, Button svs_button, string path1, string path2, string path3)
 {
-    SDL_Texture* intro_background = loadTexture("Background/intro.jpg",renderer);
-    gFont = TTF_OpenFont( "Font/race1.ttf", 28 );
-    SDL_Color textColor = {0, 200, 0};
-    SDL_Texture* title = loadFromRenderedText("SNAKE",textColor, renderer, gFont);
-    SDL_Rect rect = {SCREEN_WIDTH/3-30,0,300,100};
+    SDL_Texture *texture;
+    texture = loadTexture("Background/mode.jpg",renderer);
+    SDL_RenderCopy(renderer,texture,nullptr,nullptr);
 
-    int i = 10;
-    while (rect.y<=SCREEN_HEIGHT/3-40)
-    {
-        rect.y +=i;
-        SDL_SetRenderDrawColor(renderer,0,0,0,0);
-        SDL_RenderClear(renderer);
-        SDL_RenderCopy(renderer,intro_background,nullptr,nullptr);
-        SDL_RenderCopy(renderer,title,nullptr,&rect);
-        SDL_RenderPresent(renderer);
-        SDL_Delay(50);
-    }
+    classic_button.load_image(renderer, path1);
+    modern_button.load_image(renderer, path2);
+    svs_button.load_image(renderer, path3);
+    classic_button.draw(renderer);
+    modern_button.draw(renderer);
+    svs_button.draw(renderer);
 
-    intro_background = loadTexture("Background/play_button.png", renderer); // 246, 249, 11 (RGB)
-    SDL_Rect rect1 = {SCREEN_WIDTH/3+5,SCREEN_HEIGHT/3+60,220,100};
-    SDL_RenderCopy(renderer,intro_background,nullptr,&rect1);
     SDL_RenderPresent(renderer);
 
-    SDL_DestroyTexture(intro_background);
-    intro_background = nullptr;
-    SDL_DestroyTexture(title);
-    title = nullptr;
+    SDL_DestroyTexture(texture);
+    texture = nullptr;
 }
 
 int main (int argc, char* argv[])
@@ -66,35 +34,144 @@ int main (int argc, char* argv[])
 
     intro();
 
-    cout << "Choose mode: " << endl;
-    cout << "1: Classic" << endl;
-    cout << "2: Modern" << endl;
-    cout << "3: Snake vs Snake" << endl;
+    Button play_button;
+    play_button.set_position(SCREEN_WIDTH/3+5,SCREEN_HEIGHT/3+60,220,100);
+    play_button.load_image(renderer,"Background/over_play_button.png");
+    play_button.draw(renderer);
+    SDL_RenderPresent(renderer);
 
-    SDL_Event e;
-    while (true) {
-        if (SDL_PollEvent(&e)==0) continue;
-        else if (e.type == SDL_QUIT) break;
-        else if (e.type == SDL_KEYDOWN) {
-          switch (e.key.keysym.sym)
-          {
-              case SDLK_1: classic(); break;
-              case SDLK_2: modern(); break;
-              case SDLK_3: snake_vs_snake(); break;
-              default: break;
-          }
-          break;
+
+    bool game_is_running = true;
+    bool choose_mode = false;
+    bool check_classic = false;
+    bool check_modern = false;
+    bool check_svs = false;
+    while (game_is_running) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e) && choose_mode == false)
+        {
+            if (e.type == SDL_QUIT) game_is_running = false;
+            else
+            {
+                play_button.handle_event(e);
+                if (play_button.current_state == MOUSE_OUT)
+                {
+                    SDL_SetRenderDrawColor(renderer,255,255,255,255);
+                    SDL_RenderClear(renderer);
+                    out_over_mouse_intro(play_button,"Background/out_play_button.png");
+                }
+                else if (play_button.current_state == MOUSE_OVER)
+                {
+                    SDL_SetRenderDrawColor(renderer,255,255,255,255);
+                    SDL_RenderClear(renderer);
+                    out_over_mouse_intro(play_button,"Background/over_play_button.png");
+                }
+                else if (play_button.current_state == MOUSE_DOWN)
+                {
+                    SDL_SetRenderDrawColor(renderer,255,255,255,255);
+                    SDL_RenderClear(renderer);
+                    down_mouse_intro(play_button);
+                }
+                else if (play_button.current_state == MOUSE_UP)
+                {
+                    choose_mode = true;
+                    break;
+                }
+                SDL_RenderPresent(renderer);
+            }
         }
-    }
 
+        if (choose_mode == true) {
+            Button classic_button;
+            Button modern_button;
+            Button svs_button;
+            classic_button.set_position(150,160,300,60);
+            modern_button.set_position(150,240,300,60);
+            svs_button.set_position(150,320,300,60);
+            mode(classic_button,modern_button,svs_button,OUT_CLASSIC_BUTTON, OUT_MODERN_BUTTON, OUT_SVS_BUTTON);
+
+            while (SDL_PollEvent(&e)) {
+                if (e.type == SDL_QUIT) game_is_running = false;
+                else
+                {
+                    classic_button.handle_event(e);
+                    modern_button.handle_event(e);
+                    svs_button.handle_event(e);
+
+                    SDL_SetRenderDrawColor(renderer,255,255,255,255);
+                    SDL_RenderClear(renderer);
+
+                    if (classic_button.current_state == MOUSE_OVER)
+                    {
+                        classic_button.set_position(150,160,300,60);
+                        mode(classic_button,modern_button,svs_button,OVER_CLASSIC_BUTTON,OUT_MODERN_BUTTON,OUT_SVS_BUTTON);
+                    }
+                    else if (modern_button.current_state == MOUSE_OVER)
+                    {
+                        modern_button.set_position(150,240,300,60);
+                        mode(classic_button,modern_button,svs_button,OUT_CLASSIC_BUTTON, OVER_MODERN_BUTTON, OUT_SVS_BUTTON);
+                    }
+                    else if (svs_button.current_state == MOUSE_OVER)
+                    {
+                        svs_button.set_position(150,320,300,60);
+                        mode(classic_button,modern_button,svs_button,OUT_CLASSIC_BUTTON, OUT_MODERN_BUTTON, OVER_SVS_BUTTON);
+                    }
+                    else if (classic_button.current_state == MOUSE_DOWN) {
+                        classic_button.set_position(150+5,160+5,300-5,60-5);
+                        mode(classic_button,modern_button,svs_button,OVER_CLASSIC_BUTTON,OUT_MODERN_BUTTON,OUT_SVS_BUTTON);
+                    }
+                    else if (modern_button.current_state == MOUSE_DOWN) {
+                        modern_button.set_position(150+5,240+5,300-5,60-5);
+                        mode(classic_button,modern_button,svs_button,OUT_CLASSIC_BUTTON, OVER_MODERN_BUTTON, OUT_SVS_BUTTON);
+                    }
+                    else if (svs_button.current_state == MOUSE_DOWN) {
+                        svs_button.set_position(150+5,320+5,300-5,60-5);
+                        mode(classic_button,modern_button,svs_button,OUT_CLASSIC_BUTTON, OUT_MODERN_BUTTON, OVER_SVS_BUTTON);
+                    }
+                    else if (classic_button.current_state == MOUSE_UP) {
+                        check_classic = true;
+                        break;
+                    }
+                    else if (modern_button.current_state == MOUSE_UP){
+                        check_modern = true;
+                        break;
+                    }
+                    else if (svs_button.current_state == MOUSE_UP) {
+                        check_svs = true;
+                        break;
+                    }
+                    else if ( classic_button.current_state == MOUSE_OUT
+                           && modern_button.current_state == MOUSE_OUT
+                           && svs_button.current_state == MOUSE_OUT)
+                    {
+                        classic_button.set_position(150,160,300,60);
+                        modern_button.set_position(150,240,300,60);
+                        svs_button.set_position(150,320,300,60);
+                        mode(classic_button,modern_button,svs_button,OUT_CLASSIC_BUTTON,OUT_MODERN_BUTTON,OUT_SVS_BUTTON);
+                    }
+
+                }
+            }
+            if (check_classic == true) {
+                classic();
+                break;
+            }
+            else if (check_modern == true) {
+                modern();
+                break;
+            }
+            else if (check_svs == true) {
+                snake_vs_snake();
+                break;
+            }
+        }
+
+   }
 
     waitUntilKeyPressed();
     quitSDL(window, renderer, gFont);
     return 0;
 }
-
-
-
 
 
 
